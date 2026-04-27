04dca7c3986da40512870e8908c72e3e346428be
jhw52 <152086950+jhw52@users.noreply.github.com>
2026-04-28 01:30:11 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: './', // 允许在子路径下运行
+    base: '/junhuawang/', // 允许在子路径下运行
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
